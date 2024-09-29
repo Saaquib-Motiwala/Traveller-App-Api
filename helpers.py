7e156fae4a9f4c093766daca4912c58de252e868
Saaquib <saaquibmotiwala@gmail.com>
2024-09-29 15:13:46 +0530

    edited csv, helpers

--- helpers.py
+++ helpers.py
@@ -10,12 +10,16 @@ def get_states_list():
 
 def get_state_data(state_id):
     attr_df = pd.read_csv("attraction_details_2.csv")
+    state_df = pd.read_csv("states_id.csv")
+    state_name = state_df.loc[state_df["STATE_ID"] == state_id, "STATE_NAME"].values[0]
+    state_info = state_df.loc[state_df["STATE_ID"] == state_id, "STATE_INFO"].values[0]
+    state_image = state_df.loc[state_df["STATE_ID"] == state_id, "STATE_IMAGE"].values[0]
     attr_filtered = attr_df[attr_df["state_id"] == state_id]
     attr_filtered = attr_filtered[["id", "name", "images"]]
     attr_filtered["images"] = attr_filtered["images"].apply(lambda x: literal_eval(x))
     attr_filtered["images"] = attr_filtered["images"].apply(lambda x: x[0])
     attr_filtered_dict = attr_filtered.to_dict(orient="records")
-    return attr_filtered_dict
+    return {"state_data": {"state_name": state_name, "state_info": state_info, "state_image": state_image}, "attr_data": attr_filtered_dict}
 
 def get_attr_details(attr_id):
     attr_df = pd.read_csv("attraction_details_2.csv")
