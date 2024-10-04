ef399df9ceacc822a618a2403894987e76075d9e
Saaquib <saaquibmotiwala@gmail.com>
2024-10-05 00:12:18 +0530

    Refactored csv, added search in api, cleaned helpers code

--- helpers.py
+++ helpers.py
@@ -3,43 +3,50 @@ from ast import literal_eval
 import json
 import requests
 
-def get_states_list():
-    states_list = pd.read_csv("states_info.csv").to_dict(orient="split")
-    states_list = [[i[0], i[1], i[4]] for i in states_list["data"]]
-    return states_list
 
+class DataRetriever:
+    def __init__(self):
+        self.state_df = pd.read_csv("states_info.csv")
+        self.attr_df = pd.read_csv("attraction_details.csv")
 
-def get_state_data(state_id):
-    attr_df = pd.read_csv("attraction_details.csv")
-    state_df = pd.read_csv("states_info.csv")
-    filtered_state = state_df.loc[state_df["STATE_ID"] == state_id]
-    state_name = filtered_state["STATE_NAME"].values[0]
-    state_capital = filtered_state["STATE_CAPITAL"].values[0]
-    state_info = filtered_state["STATE_INFO"].values[0]
-    state_image = filtered_state["STATE_IMAGE"].values[0]
-    attr_filtered = attr_df[attr_df["state_id"] == state_id]
-    attr_filtered = attr_filtered[["id", "name", "cover_img"]]
-    attr_filtered_dict = attr_filtered.to_dict(orient="records")
-    return {"state_data": {"state_name": state_name, "state_capital": state_capital, "state_info": state_info, "state_image": state_image}, "attr_data": attr_filtered_dict}
+    def get_states_list(self):
+        states_list = self.state_df.to_dict(orient="split")
+        states_list = [[i[0], i[1], i[4]] for i in states_list["data"]]
+        return states_list
 
-def get_attr_details(attr_id):
-    attr_df = pd.read_csv("attraction_details.csv")
-    attr = attr_df.loc[attr_df["id"] == attr_id]
-    attr.loc[:, "images"] = attr["images"].apply(lambda x: literal_eval(x))
-    attr.loc[:, "tags"] = attr["tags"].apply(lambda x: literal_eval(x))
-    attr.loc[:, "reviews"] = attr["reviews"].apply(lambda x: literal_eval(x))
-    attr_dict = attr.to_dict(orient="records")[0]
-    return attr_dict
+    def get_state_data(self, state_id):
+        filtered_state = self.state_df.loc[self.state_df["STATE_ID"] == state_id]
+        state_name = filtered_state["STATE_NAME"].values[0]
+        state_capital = filtered_state["STATE_CAPITAL"].values[0]
+        state_info = filtered_state["STATE_INFO"].values[0]
+        state_image = filtered_state["STATE_IMAGE"].values[0]
+        attr_filtered = self.attr_df[self.attr_df["state_id"] == state_id]
+        attr_filtered = attr_filtered[["id", "name", "cover_img"]]
+        attr_filtered_dict = attr_filtered.to_dict(orient="records")
+        return {"state_data": {"state_name": state_name, "state_capital": state_capital, "state_info": state_info, "state_image": state_image}, "attr_data": attr_filtered_dict}
 
-def get_recommended_attr():
-    lst = [317329, 319875, 311667, 319695, 2704519, 12687575, 320061, 321437, 2697362, 1491020]
-    attr_df = pd.read_csv("attraction_details.csv")
-    recomm_attr = attr_df.loc[attr_df['id'].isin(lst)]
-    recomm_dict = recomm_attr.to_dict(orient="records")
-    return recomm_dict
+    def get_attr_details(self, attr_id):
+        attr = self.attr_df.loc[self.attr_df["id"] == attr_id]
+        attr.loc[:, "images"] = attr["images"].apply(lambda x: literal_eval(x))
+        attr.loc[:, "tags"] = attr["tags"].apply(lambda x: literal_eval(x))
+        attr.loc[:, "reviews"] = attr["reviews"].apply(lambda x: literal_eval(x))
+        attr_dict = attr.to_dict(orient="records")[0]
+        return attr_dict
+
+    def get_recommended_attr(self):
+        lst = [317329, 319875, 311667, 319695, 2704519, 12687575, 320061, 321437, 2697362, 1491020]
+        recomm_attr = self.attr_df.loc[self.attr_df['id'].isin(lst)]
+        recomm_dict = recomm_attr.to_dict(orient="records")
+        return recomm_dict
+
+    def get_search_result(self, query):
+        search_res = self.attr_df[self.attr_df["name"].str.contains(query, case=False)].to_dict(orient="records")
+        return search_res
 
 
 
 if __name__ == "__main__":
     # print(get_attr_details(25555680)["reviews"])
-    print(get_recommended_attr())
+    # print(get_recommended_attr())
+    dr = DataRetriever()
+    print(dr.get_search_result("Taj"))
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
-from helpers import get_states_list, get_state_data, get_attr_details, get_recommended_attr
+from helpers import DataRetriever
 
 app = FastAPI()
-
+dr = DataRetriever()
 
 @app.get("/")
 async def root():
@@ -13,24 +13,34 @@ async def root():
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
+
 @app.get("/states/")
 async def states():
-    states_list = get_states_list()
+    states_list = dr.get_states_list()
     return {"data": states_list}
+
 
 @app.get("/recommended_attr/")
 async def states():
-    recomm_dict = get_recommended_attr()
+    recomm_dict = dr.get_recommended_attr()
     return {"data": recomm_dict}
+
+
+@app.get("/search/{query}")
+async def search(query: str):
+    search_res = dr.get_search_result(query)
+    return {"data": search_res}
+
 
 @app.get("/states/{state_id}")
 async def states(state_id: int):
-    state_data = get_state_data(state_id)
+    state_data = dr.get_state_data(state_id)
     return {"data": state_data}
+
 
 @app.get("/attractions/{attr_id}")
 async def attractions(attr_id: int):
-    attr_details = get_attr_details(attr_id)
+    attr_details = dr.get_attr_details(attr_id)
     return {"data": attr_details}
 
 
